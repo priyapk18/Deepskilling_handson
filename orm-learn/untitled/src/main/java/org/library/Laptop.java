89309b5dac3beda23b7518245f4caa6bb347930c
priyapk18 <priyapk1718@gmail.com>
2026-07-18 19:51:18 +0530

    hibernate

---
+++ orm-learn/untitled/src/main/java/org/library/Laptop.java
@@ -0,0 +1,10 @@
+package org.library;
+
+public class Laptop {
+    public Laptop(){
+      System.out.println("LAptop constructor");
+    }
+    public void compile(){
+        System.out.println("Compiling");
+    }
+}
